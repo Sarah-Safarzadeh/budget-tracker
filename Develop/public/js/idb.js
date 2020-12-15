b5f39fb9075563286e6d257e091180954829627b
Sarah-Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2020-12-14 22:22:57 -0600

    added event listener for returned connectivity

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -40,3 +40,49 @@ function saveRecord(record) {
     // add record to your store with add method
     budgetObjectStore.add(record);
   }
+
+  function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_expense'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_expense');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function() {
+  // if there was data in indexedDb's store, let's send it to the api server
+  if (getAll.result.length > 0) {
+    fetch('/api/expenses', {
+      method: 'POST',
+      body: JSON.stringify(getAll.result),
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(serverResponse => {
+        if (serverResponse.message) {
+          throw new Error(serverResponse);
+        }
+        // open one more transaction
+        const transaction = db.transaction(['new_expense'], 'readwrite');
+        // access the new_pizza object store
+        const budgetObjectStore = transaction.objectStore('new_expense');
+        // clear all items in your store
+        budgetObjectStore.clear();
+
+        alert('All saved expenses have been submitted!');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadBudget);
